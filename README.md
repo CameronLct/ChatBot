6f45ec726e014472c59f569d352be12cd8707afc
CameronLct <cameron.leconte06@gmail.com>
2024-05-27 00:56:40 +0200

    chat bot

---
+++ README.md
@@ -0,0 +1,14 @@
+Mise en place
+
+Installation de Rasa :
+Installer Rasa en suivant la documentation officielle ou en utilisant le gestionnaire de packages approprié.
+
+Lancer Rasa en ligne de commande :
+Pour entraîner le modèle, utilisez la commande : rasa train
+Pour démarrer une session interactive avec votre bot, utilisez : rasa shell
+
+Lancer Rasa avec le bot Discord :
+Suivez les instructions détaillées dans le fichier data/stories.yml pour configurer les interactions et les scénarios de votre bot.
+
+Schéma du chatbot :
+Ouvrez le fichier chat_bot.drawio avec l'application en ligne draw.io pour visualiser le schéma de votre chatbot.
---
+++ actions/actions.py
@@ -0,0 +1,42 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet
+
+
+class ActionModifyComment(Action):
+    def name(self):
+        return "action_modify_comment"
+
+    def run(self, dispatcher, tracker, domain):
+        comment = next(tracker.get_latest_entity_values("commentaire"), None)
+        if comment:
+            dispatcher.utter_message(text="Votre commentaire a été modifié.")
+            return [SlotSet("table_reservation_comment", comment)]
+        else:
+            dispatcher.utter_message(text="Je n'ai pas compris votre commentaire. Pouvez-vous répéter ?")
+            return []
+
+
+class ActionShowReservationInfo(Action):
+    def name(self):
+        return "action_show_reservation_info"
+
+    def run(self, dispatcher, tracker, domain):
+        first_name = tracker.get_slot("reservation_name")
+        comment = tracker.get_slot("table_reservation_comment")
+        hour = tracker.get_slot("hour_reservation")
+        place = tracker.get_slot("number_place_reservation")
+        dispatcher.utter_message(text=f"Information de réservation :"
+                                      f"\nPrénom : {first_name}"
+                                      f"\nPlace: {place}"
+                                      f"\nHeure de réservation : {hour}"
+                                      f"\n Commentaire : {comment}")
+        return []
